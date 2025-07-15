302fbc2c4e133dddb2ba0e2efbfbf487dfc2030f
Mohitsharmait1994 <mohitsharmait1994@gmail.com>
2025-07-15 23:22:21 +0530

    xy

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard(){
+    return(
+        <div>
+            <h1>THis is dashboard page</h1>
+            <p>this is paragraph for dashboard page</p>
+        </div>
+    );
+}
+
+export default Dashboard;
---
+++ src/pages/Login.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import api from '../services/api';
+import { useNavigate } from 'react-router-dom';
+import '../css/Login.css';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await api.post('/admin/login', { email, password });
+      console.log(data);
+      localStorage.setItem('token',data.token);
+      navigate('/dashboard');
+    } catch (error) {
+      console.error(error.response?.data?.message || error.message);
+      alert(error.response?.data?.message || "Login failed");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2>Admin Login</h2>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
